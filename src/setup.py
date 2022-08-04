edbf98cd8d0a7ad9b8fd98fd3b66d176d1f89891
szinuccsi <44683763+szinuccsi@users.noreply.github.com>
2022-08-04 17:39:47 +0200

    repository initialized

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="persistance_helper",
+    version="0.0.1",
+    author="Bence Szinyéri",
+    author_email="szinyeribence@edu.bme.hu",
+    description="Helpers to persist objects",
+    long_description="Some python functions to persist Python objects",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.8',
+)
